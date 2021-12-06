8d43cbeeddd29531f85a3c0fa5f40809eb7337b6
Nathan <nathan.meneguz@epitech.eu>
2021-12-06 17:18:34 +0100

    [+] - Adding proper sidebar to homeview, created mapContext file

--- src/components/HomeSideBar/HomeSideBar.js
+++ src/components/HomeSideBar/HomeSideBar.js
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
-import places from '../../../places.json';
+import places from '../../places.json';
 
 const HomeSideBar = () => {
     const [sideBarList, setSideBarList] = useState([]);
     useEffect(() => {
+        setSideBarList([]);
         places.places.forEach((value, idx) => {
-            setSideBarList(...sideBarList, <div>
-                <div>{idx}</div>
+            setSideBarList(state => [...state,
+            <div className="transition ease-in-out duration-150 text-lg items-center p-5 border-b-2 hover:border-transparent border-gray-300 hover:bg-blue-600 hover:text-white">
                 <div>{value.name}</div>
-                <div>{value.lat}</div>
-                <div>{value.lng}</div>
-                </div>)
+            </div>])
         })
-    })
+    }, [])
     return (
-        <div>{sideBarList}</div>
+        <div>
+            <input type="text" placeholder="Search" className="p-3 border-b-2 w-full"/>
+            <div className="overflow-y-scroll">{sideBarList}</div>
+        </div>
     )
 }
 
