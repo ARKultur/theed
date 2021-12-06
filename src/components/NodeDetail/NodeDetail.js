bca76f62977fbf5ae250aa23939a44a588fddedb
KinoFool <nicolas.mary@epitech.eu>
2021-12-06 17:44:39 +0100

    [+~] Add the /nodes/edit/:id route with minimal display for test

---
+++ src/components/NodeDetail/NodeDetail.js
@@ -0,0 +1,11 @@
+import React, {useState, useRef, useEffect} from "react";
+
+const NodeDetail = () => {
+  return (
+    <div className={"flex w-full justify-center"}>
+      <h3>Node Detail route need to be /nodes/edit/</h3>
+    </div>
+  )
+}
+
+export default NodeDetail;
