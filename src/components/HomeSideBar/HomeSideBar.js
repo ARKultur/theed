ce1b7abe4b8dcef7081bba2fc879558437d24365
Nathan <nathan.meneguz@epitech.eu>
2021-12-06 16:35:41 +0100

    [~] - Fetching test data list and siplaying it in the sidebar

--- src/components/HomeSideBar/HomeSideBar.js
+++ src/components/HomeSideBar/HomeSideBar.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
+import places from '../../../places.json';
+
 const HomeSideBar = () => {
+    const [sideBarList, setSideBarList] = useState([]);
+    useEffect(() => {
+        places.places.forEach((value, idx) => {
+            setSideBarList(...sideBarList, <div>
+                <div>{idx}</div>
+                <div>{value.name}</div>
+                <div>{value.lat}</div>
+                <div>{value.lng}</div>
+                </div>)
+        })
+    })
     return (
-        <div>bite</div>
+        <div>{sideBarList}</div>
     )
 }
 
