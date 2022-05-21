5a76933e3a70a034ad8f87ad990b46ea13f362d6
winds0r <wontsleep@protonmail.com>
2022-05-21 19:48:15 +0200

    (mod/build)multi-stage dockerifle

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,22 @@
-FROM node:18.2.0-alpine3.12
+FROM node:18.2-alpine3.14 as builder
 
 RUN mkdir -p /theed
 WORKDIR /theed
 
 COPY package.json .
-RUN yarn global add serve && \
-    yarn install
-
+RUN yarn install
 
 COPY . .
+EXPOSE 3000
+RUN yarn build:prod
 
+
+
+FROM node:18.2-alpine3.14 as final
+
+RUN yarn global add serve
 EXPOSE 3000
 
-RUN yarn build
+COPY --from=builder /theed/dist/ .
+CMD ["serve", "-s" , "dist"]
 
-CMD ["serve", "-s" , "build"]
--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import './style.css'
+import m from "mithril";
 
-const hello = document.createElement('h1');
-const wrapper = document.createElement('div');
-
-hello.textContent = 'Hello World !';
-hello.classList.add(
-  'bg-gradient-to-r', 'from-green-400', 'to-blue-500',
-  'text-transparent', 'text-9xl', 'bg-clip-text', 'font-extrabold'
-  );
-
-wrapper.classList.add(
-  'flex', 'w-full', 'h-screen',
-  'justify-center', 'items-center'
-  )
-
-wrapper.appendChild(hello);
-document.body.appendChild(wrapper);
+m.render(document.body, "hello world");
