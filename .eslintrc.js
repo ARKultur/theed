8b875ae757ee4eda685ca23c8202c852ef81c356
winds0r <wontsleep@protonmail.com>
2022-06-14 22:02:26 +0200

    (mod/ci)linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
-  },
-  'rules': {
-  },
+    'env': {
+        'browser': true,
+        'es2021': true,
+    },
+    'extends': [
+        'google',
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+    },
+    'rules': {
+        'require-jsdoc': 0,
+    },
 };
