59d23414bf568900249a38644cef94b748aef21a
SheItoon <guillaume.debard@epitech.eu>
2022-10-05 01:27:34 +0200

    fix webpack and login button

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,11 +1,12 @@
 export const API_URL = isInProd() ? hostname() + '/api' : 'http://localhost:4000/api';
 export const WEBSITE_URL = isInProd() ? hostname() : 'http://localhost:3000';
 
+
 function hostname() {
-    return 'https://arkultur.creative-rift.com';
+    return HOST;
 }
 
 function isInProd() {
-    return true;
+    return PROD;
 }
 
