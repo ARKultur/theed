f4e36c44bac1c5c47d86b340848a485228652e4c
bogdan <wontsleep@protonmail.com>
2022-11-26 16:26:22 +0900

    Add Not Found, Maintenance page (#42)
    Add HomePage Button
    Fix footer's link
    
    Co-authored-by: Guillaume S <guillaume.soisson.69@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import HomeView from './views/Home';
 import LandingView from './views/LandingView';
 import LoginView from './views/LoginView';
 import RegisterView from './views/RegisterView';
+import NotFoundView from "./views/NotFound";
+import MaintenanceView from "./views/Maintenance";
 
 m.route.prefix ="";
+
 
 m.route(
     document.body,
@@ -14,7 +17,9 @@ m.route(
     '/': LandingView,
     '/home': HomeView,
     '/login': LoginView,
-    '/register': RegisterView
+    '/register': RegisterView,
+    '/maintenance': MaintenanceView,
+    '/:404': NotFoundView
 },
 );
 
---
+++ src/views/Maintenance.js
@@ -0,0 +1,48 @@
+import m from 'mithril';
+import TopbarComponent from "../components/Bars/Topbar";
+import FooterComponent from "../components/FooterComponent";
+import HomePageButton from "../components/Buttons/HomePageButton";
+
+const MaintenanceView = {
+    view: function (_vnode) {
+        return m('root', [
+            m(TopbarComponent),
+            m('section', {
+                'class': 'bg-white dark:bg-gray-900'
+            },
+                m('div', {
+                    'class': 'grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28'
+                },
+                    [
+                        m('div', {
+                                'class': 'mr-auto place-self-center lg:col-span-7',
+                            },
+                            m('h1', {
+                                'class': 'max-w-2xl mb-4 text-4xl font-extrabold leading-none ' +
+                                    ' tracking-tight md:text-5xl xl:text-6xl dark:text-white'
+                            }, "Maintenance"),
+                            m('p', {
+                                'class': 'max-w-2xl mb-6 font-light text-gray-500 lg:mb-8' +
+                                    ' md:text-lg lg:text-xl dark:text-gray-400'
+                            }, "This page is currently in maintenance. Sorry for the inconvenience."),
+                            m('div', {
+                                'class': 'space-y-4 sm:flex sm:space-y-0 sm:space-x-4',
+                            },
+                                m(HomePageButton)
+                            )
+                        ),
+                        m('div', {
+                            'class': 'hidden lg:mt-0 lg:col-span-5 lg:flex',
+                        }, m('img', {
+                                'src': 'public/images/stylish-logo.svg',
+                                'alt': 'stylish logo'
+                            }
+                        ))
+                    ]
+                )),
+            m(FooterComponent)
+        ]);
+    }
+}
+
+export default MaintenanceView;
